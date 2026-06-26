08d5dfc18259c1c3fee8338e9d645519d95531b9
DoanVan-arch <skypro311294@gmail.com>
2026-06-26 11:22:58 +0700

    modified:   app/routes/nomination.py

--- app/routes/nomination.py
+++ app/routes/nomination.py
@@ -349,6 +349,7 @@ def edit_nomination(id):
         DeXuatChiTiet.quan_nhan_id.isnot(None),
         DeXuat.id != de_xuat.id,
         DeXuat.trang_thai != TrangThaiDeXuat.NHAP.value,
+        DeXuatChiTiet.trang_thai != TrangThaiDeXuat.TU_CHOI.values,
     ).all()
     already_in_other = set(row[0] for row in already_in_other_q)
 
